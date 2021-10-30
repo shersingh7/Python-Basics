f4ea69be7308cba909122f6995d57d5b445eef7b
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-10-30 09:09:31 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,106 @@
+# print("Hello World\nI am Davinder Verma.")
+# print("*" * 20)
+
+
+# # Variables
+#
+# price = 10
+# print("The price of the apple is " + str(price))
+
+
+# # Exercise
+#
+# name = "John Smith"
+# age = 20
+# newPatient = True
+#
+# print(name)
+# print(age)
+# if(newPatient == True): print("New Patient")
+#
+# else:  print("Returning Patient")
+#
+# print("\n\n\n\n")
+
+
+# # Taking input
+#
+# # name = input("What is your name? ")
+# # print("Hi " + name)
+
+
+# # Exercise
+#
+# # name = input("What is your name? ")
+# # color = input("What is your favourite color? ")
+# # print(name + " likes " + color)
+
+
+# # Calculate the age
+#
+# # birth_year = int(input("Birth year: "))
+# # age = 2021 - birth_year
+# # print("Your age is " + str(age))
+
+
+# #Get input in punds and convert in kg
+#
+# pounds = input("Weight (Pounds): ")
+# kg = 0.45 * int(pounds)
+# print("The weight in kg is " + str(kg))
+
+
+# Email
+# email = '''
+# Hi there,
+#
+# Here is my first email to you.
+#
+# Thank You,
+# Davinder Verma
+#
+# '''
+
+# print(email)
+
+
+# Print whole array
+
+# course = "Python for beginners"
+# another = course[:]
+#
+# print(another)
+
+
+# # Formatted Strings
+#
+# first_name = "Davinder"
+# last_name = "Verma"
+#
+# msg = f'{first_name} [{last_name}] is a coder'
+#
+# print(msg)
+
+
+# # Print the number of length of the string
+#
+#course = "Python for beginners"
+#
+# print(len(course))
+## Find the index of the character
+#print(course.find("b"))
+
+## Find if the character is in the string (Case Sensitive)
+
+# course = "Python for beginners"
+# print('Python' in course)
+
+
+
+
+
+
+
+
+
+
